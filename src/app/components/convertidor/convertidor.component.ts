43fb751208f784c02ab21374551dd1e9b115a387
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-02-04 21:48:36 -0500

    Convertidor

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -6,10 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./convertidor.component.css']
 })
 export class ConvertidorComponent implements OnInit {
+  cantidad = 0;
+  tengo = 'USD';
+  quiero = 'MLC'
+  total = 0
+  monedas: string[] = ['USD', 'MLC', 'CUP'];
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertir() {
+    switch (this.tengo) {
+      case 'USD':
+        if (this.quiero === 'CUP') {
+          this.total = this.cantidad * 295;
+
+        } else if (this.quiero === 'MLC') {
+          this.total = this.cantidad * 0.86
+
+        } else {
+          this.total = this.cantidad;
+
+        }
+        break;
+      case 'MLC':
+        if (this.quiero === 'CUP') {
+          this.total = this.cantidad * 260;
+
+        } else if (this.quiero === 'USD') {
+          this.total = this.cantidad * 0.90
+
+        } else {
+          this.total = this.cantidad;
+
+        }
+        break;
+      case 'CUP':
+        if (this.quiero === 'MLC') {
+          this.total = this.cantidad / 260;
+
+        } else if (this.quiero === 'USD') {
+          this.total = this.cantidad / 295;
+
+        } else {
+          this.total = this.cantidad;
+
+        }
+        break;
+    }
   }
 
 }
